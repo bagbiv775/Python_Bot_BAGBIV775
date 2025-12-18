d837260073369538d111b9bf405f8e7eb1403265
bagbiv775 <bagbiv@gmail.com>
2025-12-18 19:25:43 +0500

    add_Max_and_other_users_choice

--- main.py
+++ main.py
@@ -12,11 +12,15 @@ dp = Dispatcher()
 @dp.message(Command('start'))
 async def start_command(message):
     print(message)
+    print(message.text)
 
 
 @dp.message()
 async def all_messages(message: Message, bot: Bot):
-    msg_text = f'Пользователь {message.from_user.full_name} написал:\n{message.text}'
+    if message.from_user.full_name == 'Max':
+        msg_text = f'Хватит баловаться, {message.from_user.full_name}!!!\nИди учи уроки!!!'
+    else:
+        msg_text = f'Пользователь {message.from_user.full_name} написал:\n{message.text}'
 
     await bot.send_message(message.from_user.id, msg_text) # я так сделал
 
