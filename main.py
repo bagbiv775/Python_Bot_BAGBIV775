15512c581a68b8b8f5366494c35d0772ed90f588
bagbiv775 <bagbiv@gmail.com>
2025-12-18 18:17:55 +0500

    initial_commit

---
+++ main.py
@@ -0,0 +1,34 @@
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
+import asyncio
+
+import config
+
+bot = Bot(token=config.BOT_TOKEN)
+dp = Dispatcher()
+
+
+@dp.message(Command('start'))
+async def start_command(message):
+    print(message)
+
+
+@dp.message()
+async def all_messages(message: Message, bot: Bot):
+    msg_text = f'Пользователь {message.from_user.full_name} написал:\n{message.text}'
+
+    await bot.send_message(message.from_user.id, msg_text) # я так сделал
+
+    # await bot.send_message( # STONE так сделал
+    #     message.chat.id,
+    #     message.text,
+    # )
+
+
+async def start_bot():
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    asyncio.run(start_bot())
