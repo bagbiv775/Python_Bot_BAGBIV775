4721caec76eefa9c2e5af0ac00f633a270ccb70b
bagbiv775 <bagbiv@gmail.com>
2025-12-30 00:00:39 +0500

    made_one_common_handler_and_add_images_in_menu

--- handlers.py
+++ handlers.py
@@ -1,5 +1,5 @@
 from aiogram import Router, Bot
-from aiogram.filters import Command
+from aiogram.filters import Command, CommandObject
 from aiogram.types import Message
 from aiogram.types.input_file import FSInputFile
 
@@ -13,23 +13,55 @@ from utils.enum_path import Path
 main_router = Router()
 
 
-@main_router.message(Command('start'))
-async def start_command(message: Message):
-    await message.answer(
-        text=FileManager.read_txt(Path.MESSAGES, 'main.txt'),
+@main_router.message(Command('start', 'random', 'gpt', 'talk', 'quiz'))
+async def command_handler(message: Message, command: CommandObject):
+    await message.answer_photo(
+        photo=FSInputFile(Path.IMAGES.value.format(file=command.command)),
+        caption=FileManager.read_txt(Path.MESSAGES, command.command),
         reply_markup=keyboard_main_menu(),
     )
 
 
-@main_router.message(Command('random'))
-async def random_command(message: Message):
-    print(message.from_user.id)
+# @main_router.message(Command('random'))
+# async def random_command(message: Message):
+#     await message.answer_photo(
+#         photo=FSInputFile(Path.IMAGES.value.format(file='random')),
+#         caption=FileManager.read_txt(Path.MESSAGES, 'random'),
+#         reply_markup=keyboard_main_menu(),
+#     )
+#
+#
+# @main_router.message(Command('gpt'))
+# async def gpt_command(message: Message):
+#     await message.answer_photo(
+#         photo=FSInputFile(Path.IMAGES.value.format(file='gpt')),
+#         caption=FileManager.read_txt(Path.MESSAGES, 'gpt'),
+#         reply_markup=keyboard_main_menu(),
+#     )
+#
+#
+# @main_router.message(Command('talk'))
+# async def talk_command(message: Message):
+#     await message.answer_photo(
+#         photo=FSInputFile(Path.IMAGES.value.format(file='talk')),
+#         caption=FileManager.read_txt(Path.MESSAGES, 'talk'),
+#         reply_markup=keyboard_main_menu(),
+#     )
+#
+#
+# @main_router.message(Command('quiz'))
+# async def quiz_command(message: Message):
+#     await message.answer_photo(
+#         photo=FSInputFile(Path.IMAGES.value.format(file='quiz')),
+#         caption=FileManager.read_txt(Path.MESSAGES, 'quiz'),
+#         reply_markup=keyboard_main_menu(),
+#     )
 
 
-@main_router.message()
-async def all_messages(message: Message, bot: Bot):
-    msg_text = f'Пользователь {message.from_user.full_name} написал:\n{message.text}'
-    await bot.send_message(
-        chat_id=config.ADMIN_ID,
-        text = msg_text,
-    )
+# @main_router.message()
+# async def all_messages(message: Message, bot: Bot):
+#     msg_text = f'Пользователь {message.from_user.full_name} написал:\n{message.text}'
+#     await bot.send_message(
+#         chat_id=config.ADMIN_ID,
+#         text = msg_text,
+#     )
