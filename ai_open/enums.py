b40d56b02801035a685cd78feaba69d78d7892ac
bagbiv775 <bagbiv@gmail.com>
2026-01-04 17:21:57 +0500

    final_commit

--- ai_open/enums.py
+++ ai_open/enums.py
@@ -8,7 +8,8 @@ class GPTRole(Enum):
 
 
 class GPTModel(Enum):
-    GPT_3_TURBO = 'gpt-3.5-turbo'
-    GPT_4_TURBO = 'gpt-4-turbo'
+    # GPT_3_TURBO = 'gpt-3.5-turbo'
+    # GPT_4_TURBO = 'gpt-4-turbo'
+    GPT_4_MINI = 'gpt-4o-mini'
     WHISPER = 'whisper-1' # для преобразования голоса в текст
     GPT_IMAGE = 'dall-e-3' # для создания картинок
